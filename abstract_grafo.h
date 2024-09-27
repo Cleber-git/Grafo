dc8d227471bb973d6df79a2ad546a823f2e2339b
Cleber-git <cleber192875@gmail.com>
2024-09-27 20:15:09 -0300

    Remontando a classe abstrata

--- abstract_grafo.h
+++ abstract_grafo.h
@@ -1,4 +1,18 @@
 #ifndef ABSTRACT_GRAFO_H
 #define ABSTRACT_GRAFO_H
 
+#include <QVector>
+
+class AbstractGrafo
+{
+public:
+    virtual void adicionarAresta(int _v1, int _v2) = 0;
+    virtual void mostrarGrafo() = 0;
+    virtual void resizeAdj(int _v) = 0;
+    virtual bool ifExist(int _value, QVector<int> _vertice) = 0;
+    virtual int getSizeAdjacencia() = 0;
+
+
+
+};
 #endif // ABSTRACT_GRAFO_H
--- grafo.h
+++ grafo.h
@@ -1,21 +1,21 @@
 #ifndef GRAFO_H
 #define GRAFO_H
-#include <QVector>
-#include <QTextEdit>
-
 #include "abstract_grafo.h"
 
+#include <QVector>
+#include <QTextEdit>
 // Grafo simples
-class Grafo : AbstractGrafo
+class Grafo : public AbstractGrafo
 {
 public:
     Grafo(int V);
-
+    void mostrarGrafo()override;
     void adicionarAresta(int _v1, int _v2)override;
-    void showGrafo(QTextEdit *_textEdit );
+    void showGrafo(QTextEdit *_textEdit);
     void resizeAdj(int _v)override;
-    int getAdjacencia()override{return adjacencias.size();};
-    bool isExist(int _value, QVector<int> _vertice)override;
+    int getSizeAdjacencia()override{return adjacencias.size();}
+    bool ifExist(int _value, QVector<int> _vertice)override;
+
 private:
     int NumeroVertices;
     QVector<QVector<int>> adjacencias;
