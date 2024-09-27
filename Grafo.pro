763c13b5bcc7ad452b7194c12a65308b66ec100a
cleber-git <cleber192875@gmail.com>
2024-09-27 17:00:13 -0300

    criando classe abstrata de grafo, para futuramente criar outros tipos de grafo e aplicar os conceitos de fabrica abstrata/fabrica de objetos

--- Grafo.pro
+++ Grafo.pro
@@ -14,6 +14,7 @@ SOURCES += \
     mainwindow.cpp
 
 HEADERS += \
+    abstract_grafo.h \
     grafo.h \
     mainwindow.h
 
--- grafo.h
+++ grafo.h
@@ -3,17 +3,19 @@
 #include <QVector>
 #include <QTextEdit>
 
+#include "abstract_grafo.h"
+
 // Grafo simples
-class Grafo
+class Grafo : AbstractGrafo
 {
 public:
     Grafo(int V);
 
-    void adicionarAresta(int v1, int v2);
+    void adicionarAresta(int _v1, int _v2)override;
     void showGrafo(QTextEdit *_textEdit );
-    void resizeAdj(int V);
-    int getAdjacencia(){return adjacencias.size();};
-    bool isExist(int _value, QVector<int> _vertice);
+    void resizeAdj(int _v)override;
+    int getAdjacencia()override{return adjacencias.size();};
+    bool isExist(int _value, QVector<int> _vertice)override;
 private:
     int NumeroVertices;
     QVector<QVector<int>> adjacencias;
