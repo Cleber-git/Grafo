291c9689f1b949cbf58027f944a26fa15dee29ba
Cleber-git <cleber192875@gmail.com>
2024-11-03 15:46:15 -0300

    Criar branch develop

--- abstract_grafo.h
+++ abstract_grafo.h
@@ -13,6 +13,5 @@ public:
     virtual void mostrarGrafo(QTextEdit* _textEdit) = 0;
     virtual void resizeAdj(int _v) = 0;
     virtual bool ifExist(int _value, QVector<int> _vertice) = 0;
-    virtual  int getSizeAdjacencia() = 0;
-};
+    virtual  int getSizeAdjacencia() = 0;};
 #endif // ABSTRACT_GRAFO_H
